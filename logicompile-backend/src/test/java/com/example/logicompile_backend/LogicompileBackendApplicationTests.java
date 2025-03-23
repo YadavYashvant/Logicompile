7b41a40c5d1bf581d32107cbd1a072f62fadadd2
YadavYashvant <yashvantyadav855@gmail.com>
2025-03-23 13:20:34 +0530

    added backend

---
+++ logicompile-backend/src/test/java/com/example/logicompile_backend/LogicompileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logicompile_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicompileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
